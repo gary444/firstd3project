601e1d01309769a11616079fff6025c4e6958fa0
Rendle <gareth.rendle@capgemini.com>
2018-07-23 16:56:32 +0200

    loads players into 2 arrays

--- loadjson.js
+++ loadjson.js
@@ -1,10 +1,9 @@
+//create pitch
 const pitch_width = 800;
 const pitch_length = 500;
-
 let svgContainer = d3.select("body").append("svg")
   .attr("width", pitch_width)
   .attr("height", pitch_length);
-
 let pitch = svgContainer.append("rect")
   .attr("x", 0)
   .attr("y", 0)
@@ -19,26 +18,50 @@ let pitchXScale = d3.scaleLinear()
 let pitchYScale = d3.scaleLinear()
   .domain([0,80])
   .range([0, pitch_length]);
-
 //gives line correct colour
 function lineColor(d){
   if (d.team.id === 746) {return "#5de2ce";} //man city
   else if (d.team.id === 745) {return "#1a26ce";} //chelsea
 }
 
+//load player list
+let team1_players = [];
+let team2_players = [];
+d3.json("data/lineups_7298.json")
+.then(function (data) {
 
+  //for each player in each team
+  for (let team=0; team < data.length; team++){
+    for (let p = 0; p < data[team].lineup.length; p++){
+        // console.log(data[team].lineup[player].player_name);+
+        let this_player = data[team].lineup[p];
+        //add some fields
+        this_player.mins_played = 90;
+        // this_player.passes_played = 0;
+
+        if (team === 0) {team1_players.push(this_player);}
+        else if (team === 1) {team2_players.push(this_player);}
+    }
+  }
+
+
+  console.log(team1_players);
+
+});
+
+
+
+//load passes
 d3.json("data/events_7298.json")
 .then(function(data) {
-
   let passes = [];
-
   for (var i = 0; i < data.length; i++) {
     //create shorter array from only passes
     if (data[i].type.name === "Pass"){
       passes.push(data[i]);
     }
   }
-
+  //draw passes as lines on the pitch
   svgContainer.selectAll("line")
     .data(passes)
     .enter().append("line")
